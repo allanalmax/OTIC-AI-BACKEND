5f17588434ced439eac30967a530ef05506c9901
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-08-22 15:05:21 +0300

    register

--- Accounts/views.py
+++ Accounts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home (request):
-    return render(request,'home.html')
+    return render(request,'login.html')
+
+def signup (request):
+    return render(request,'signup.html')
--- OTICAI/settings.py
+++ OTICAI/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'OTICAI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
-DATABASES = {
+}
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'somesai_personalised_learning',
@@ -90,7 +90,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

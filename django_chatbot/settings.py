ecdc3662277e00bb85f80331fdd0566cb9767321
mugumbyabenon <mugumbyabenon03@gmail.com>
2023-08-23 14:21:36 +0300

    test

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -92,7 +92,6 @@ DATABASES = {
     }
 }
 
-CSRF_TRUSTED_ORIGINS = ['https://somesai-personalized-learning.azurewebsites.net']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

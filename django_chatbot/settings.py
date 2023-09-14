e954d8bd6a559dab5d64adca37275c448c43f280
mugumbyabenon <mugumbyabenon03@gmail.com>
2023-09-14 11:08:15 +0300

    test

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'django_chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -94,8 +94,7 @@ DATABASES = {
         'HOST': '34.29.52.253',
         'PORT': '5432',
     }
-}'''
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
